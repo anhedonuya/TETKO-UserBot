04e56c69a25e46477c2cab660b398b3c24f28f57
anhedonuya <tetkouserbot@gmail.com>
2026-09-18 19:54:52 +1000

    checkpoint before cleanup.

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd ~/TETKO_developing
-export TETKO_API_ID=$(python3 -c "import json; print(json.load(open('config.json'))['api_id'])")
-export TETKO_API_HASH=$(python3 -c "import json; print(json.load(open('config.json'))['api_hash'])")
-export TETKO_SESSION=$(python3 -c "import json; print(json.load(open('config.json')).get('session_name', 'tetko'))")
-exec python run_tetko.py
+exec python main.py
